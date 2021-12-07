334226fa764152373764563e60e63eb43aacec49
dimitrijus <dimitrijus@coral.lt>
2021-12-08 00:13:36 +0300

    fix

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,12 +6,15 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
+    //constructor(props) {
+        //super(props);        
+    //}
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             //debugger;        
             this.props.setUsers(response.data.items);
-        });      
+        });   
     }
 
     render() {
