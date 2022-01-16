889447e44bc3b86d2c177006f30114232b15f71a
dimitrijus <dimitrijus@coral.lt>
2022-01-17 00:47:32 +0300

    85 lesson

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // react увидит объект и сохранит его в памяти, он живет и можно внутри хранить стэйт локальный
 const ProfileStatusWithHooks = (props) => {
 
-    let [editMode, setEditMode]  = useState(false);
+    let [editMode, setEditMode]  = useState(false); // хук возвр массив из двух элементов
+// переменная инициал-я в локальном стэйте и функция которая будет менять её значение - за этой функ-цией следит реакт
+// если менять этой функ-ей переменную - то будет запускаться цикл рендеринга 
+
     let [status, setStatus]  = useState(props.status);
+
+    useEffect( () => { // хук - выполняется посл отрисовки
+        //debugger;
+        setStatus(props.status);
+    }, [props.status] ); // пустой массив - отрисуйся один раз, но так незя
 
     const activateEditMode = () => {
         setEditMode(true);
