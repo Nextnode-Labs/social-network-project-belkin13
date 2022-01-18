6879fb2dd4f92039d18ba2aa1ce1449013183ed1
dimitrijus <dimitrijus@coral.lt>
2022-01-19 01:12:17 +0300

    lesson 87

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import { Field, reduxForm } from 'redux-form';
@@ -26,24 +26,39 @@ let AddNewPostForm = (props) => {
 
 let AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"}) (AddNewPostForm);
 
-const MyPosts = (props) => {
+//class MyPosts extends PureComponent {
 
-  let onAddPost = (values) => {
-    props.addPost(values.newPostText);
-  }
+const MyPosts = React.memo(props => {
+  //componentDidUpdate(prevProps, prevState, snapshot) {
+   // console.log("componentDidMount");
+  //}
 
-  let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} key={p.id} />);
+  //shouldComponentUpdate(nextProps, nextState) { // ответит нужно ли перерисовываться - если false - то не нужно
+    //return nextProps != this.props || nextState != this.state;
+  //}
 
-  return (
-    <div className={s.postsBlock}>
-      <h3>My posts</h3>
-      <AddNewPostFormRedux onSubmit={onAddPost} />
-      <div className={s.posts}>
-        { postsElements }
+  console.log("RENDER YOU");
+ //render() {
+    
+    
+    let onAddPost = (values) => {
+      props.addPost(values.newPostText);
+    }
+
+    let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} key={p.id} />);
+
+    return (
+      <div className={s.postsBlock}>
+        <h3>My posts</h3>
+        <AddNewPostFormRedux onSubmit={onAddPost} />
+        <div className={s.posts}>
+          { postsElements }
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  
+
+});
 
 
 
