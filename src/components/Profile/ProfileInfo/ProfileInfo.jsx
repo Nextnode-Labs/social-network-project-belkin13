678b55b0a9a14a76af579ae62c897bc2590a0825
Dimitrijus <dimitrijus@coral.lt>
2022-01-28 17:55:55 +0300

    fix

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 import userPhoto from '../../../assets/images/user.png';
 
 const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
+
+  let [editMode, setEditMode]  = useState(false);
 
   if (!profile) {
     return <Preloader />
@@ -21,10 +23,51 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
       <div className={s.descriptionBlock}>
         <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
         { isOwner && <input type={"file"} onChange={onMainPhotoSelected}/> }
+
+        { editMode 
+        ? <ProfileDataForm profile={profile} /> 
+        : <ProfileData goToEditMode = {() => {setEditMode(true)}} profile={profile} isOwner={isOwner} />}
+
         <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
       </div>
     </div>
   )
+}
+
+const ProfileData = ({profile, isOwner, goToEditMode}) => {
+  return <div>
+    {isOwner && <div><button onClick={goToEditMode}>edit</button></div>}
+            <div>
+              <b>Full name</b>: {profile.fullName}  
+            </div>
+            <div>
+              <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no" }  
+            </div>
+            { profile.lookingForAJob && 
+              <div>
+                <b>My professional skills</b>: {profile.lookingForAJobDescription }  
+              </div>
+            }  
+            <div>
+              <b>About me</b>: {profile.aboutMe}  
+            </div>
+            <div>
+              <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+              })}  
+            </div>
+          </div>
+}
+
+const ProfileDataForm = ({profile}) => {
+          <div>   
+            form
+          </div>
+}
+
+
+const Contact = ({contactTitle, contactValue}) => {
+  return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>
 }
 
 export default ProfileInfo;
